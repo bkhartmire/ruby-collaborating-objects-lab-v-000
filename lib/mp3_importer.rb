2c42394c69c3d002bf623d763f3966f6d4087853
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-29 04:49:40 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,6 +6,10 @@ class MP3Importer
   end
 
   def import
+<<<<<<< HEAD
     @files.each {|file| Song.new_by_filename(file)}
+=======
+    self.files.each {|file| Song.new_by_filename(file)}
+>>>>>>> 2eb644214c0bca3bb6d345980a6abb6c240e7e05
   end
 end
